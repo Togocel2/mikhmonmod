ecfebb77d4af39125bedc68b71748c645cdfa807
Togocel2 <bogambo1993@gmail.com>
2024-08-26 09:57:15 +0000

    Initial commit

---
+++ src/include/config.php
@@ -0,0 +1,8 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<admin','mikhmon>|>qqChrA==');
+
+
+
+
+$data['azre'] = array ('1'=>'azre!192.168.10.175','azre@|@admin','azre#|#pZKlrJak','azre%yadr','azre^tdsy','azre&CFA','azre*10','azre(1','azre)','azre=10','azre@!@disable');
